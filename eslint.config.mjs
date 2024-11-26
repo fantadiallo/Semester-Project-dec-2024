ce1375c4f913ca73342f8965d0dfe66c21d29ad4
fantadiallo <fatmbo0003@outlook.com>
2024-11-26 14:47:34 +0100

    Added HTML files, configured Prettier, updated lock file, and integrated Bootstrap HTML structure across all pages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
 /** @type {import('eslint').Linter.Config[]} */
+import pluginJs from "eslint-plugin-js";
+import globals from "globals";
+
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        describe: true,
+        test: true,
+        it: true,
+        expect: true,
+        require: true,
+        module: true,
+        process: true,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
