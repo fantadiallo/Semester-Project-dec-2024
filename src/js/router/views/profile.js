0685870d3b6e574d3677a1f37dd7bff9b41e1850
fantadiallo <128003934+fantadiallo@users.noreply.github.com>
2025-01-24 18:03:35 +0100

    fix:update

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,20 +1,16 @@
-
 import { onUpdateProfile } from "../../ui/profile/update.js";
 import { readProfile } from "../../ui/read/read.js";
 import { authGuard } from "../../utilities/authGuard.js";
 import { populateUpdateForm } from "../../utilities/cards/populate.js";
 import { createProfileCard } from "../../utilities/cards/profileCard.js";
 
-
 const form = document.forms.updateProfile; // Target the form by name
 if (form) {
   form.addEventListener("submit", onUpdateProfile);
 }
 
-
-onUpdateProfile()
-authGuard()
+onUpdateProfile();
+authGuard();
 readProfile();
-populateUpdateForm()
-createProfileCard()
-
+populateUpdateForm();
+createProfileCard();
