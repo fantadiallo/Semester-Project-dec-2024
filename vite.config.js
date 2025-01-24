b68c8207bf96ae7d7a472d8a734ae2a68562dd3b
fantadiallo <128003934+fantadiallo@users.noreply.github.com>
2025-01-24 16:19:15 +0100

    fix: vite

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,23 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  root: ".",
+  appType: "mpa",
   build: {
-    outDir: "dist", 
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),         
         login: path.resolve(__dirname, "auth/login/index.html"), 
         register: path.resolve(__dirname, "auth/register/index.html"), 
-        profile: path.resolve(__dirname,"profile/index.html"),
-        hompage: path.resolve(__dirname, "homepage/index.html"),
-        about: path.resolve(__dirname,"about/index.html"),
+        profile: path.resolve(__dirname, "profile/index.html"),
+        homepage: path.resolve(__dirname, "homepage/index.html"),
+        about: path.resolve(__dirname, "about/index.html"),
         contact: path.resolve(__dirname, "contact/index.html"),
         details: path.resolve(__dirname, "details/index.html"),
         notFound: path.resolve(__dirname, "notFound/index.html"),
-        createListing: path.resolve(__dirname, "createListing/index.html")
+        createListing: path.resolve(__dirname, "createListing/index.html"),
       },
-    
     },
+    
   },
   server: {
     open: "/index.html", 
@@ -34,10 +33,11 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @import "src/scss/_variables.scss"; // Include global SCSS variables
-          @import "bootstrap/scss/bootstrap"; // Include Bootstrap SCSS
+          @use "src/scss/base/variables" as *; // Make variables globally available
+          @use "src/scss/layout/mixins" as *; // Make mixins globally available
         `,
       },
     },
   },
+  
 });
